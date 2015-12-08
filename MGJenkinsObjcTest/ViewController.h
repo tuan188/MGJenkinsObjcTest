e32905c0cdd10caedfce73d31476a400cb2fab26
Truong Anh Tuan <tuan188@gmail.com>
2015-12-08 10:41:30 +0700

    Initial Commit

---
+++ MGJenkinsObjcTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MGJenkinsObjcTest
+//
+//  Created by Tuan Truong on 12/8/15.
+//  Copyright © 2015 Tuan Truong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
